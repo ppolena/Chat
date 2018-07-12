6abe5c12db53714916a4817673821d27f72e20cb
Polena Péter <PPolena92@gmail.com>
2018-07-12 10:28:05 +0200

    Initial commit

---
+++ src/main/java/hello/SocketHandler.java
@@ -0,0 +1,31 @@
+package hello;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+public class SocketHandler extends TextWebSocketHandler {
+
+    List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message)
+            throws InterruptedException, IOException {
+
+        for(WebSocketSession webSocketSession : sessions) {
+            webSocketSession.sendMessage(new TextMessage(message.getPayload()));
+        }
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        //the messages will be broadcasted to all users.
+        sessions.add(session);
+    }
+}
